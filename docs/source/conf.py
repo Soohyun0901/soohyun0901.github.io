ab0b55a99ac7227f12cb772b582d2380d0e5a218
soohyun <soohyun000901@gmail.com>
2025-03-12 22:45:47 +0900

    5.클러스터관리자수정중

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,7 @@ exclude_patterns = []
 
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 html_favicon='virton-favicon.ico'
 html_logo='virton-logo.svg'
 
